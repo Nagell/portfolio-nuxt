fa6b1bf3e891f5c2763d629061e770d3517b1e52
Dawid <dawidnitka@gmail.com>
2024-11-05 21:50:11 +0100

    feat: add nuxt-lucide-icons package and include in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
         '@nuxtjs/supabase',
         'shadcn-nuxt',
         '@nuxtjs/color-mode',
+        'nuxt-lucide-icons'
     ],
     supabase: {
         redirectOptions: {
