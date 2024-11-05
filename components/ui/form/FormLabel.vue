4e518a6dd08ad9d929a4b3adaceeccf9898c5efd
Dawid <dawidnitka@gmail.com>
2024-11-05 21:53:50 +0100

    feat: add form components and input utilities

---
+++ components/ui/form/FormLabel.vue
@@ -0,0 +1,26 @@
+<template>
+    <Label
+        :class="cn(
+            error && 'text-destructive',
+            props.class,
+        )"
+        :for="formItemId"
+    >
+        <slot />
+    </Label>
+</template>
+
+<script lang="ts" setup>
+
+    import { Label } from '@/components/ui/label'
+    import { cn } from '@/lib/utils'
+
+    import { useFormField } from './useFormField'
+
+    import type { LabelProps } from 'radix-vue'
+    import type { HTMLAttributes } from 'vue'
+
+    const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>()
+
+    const { error, formItemId } = useFormField()
+</script>
