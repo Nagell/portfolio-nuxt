fa71d6a5ac5653e14df9b7fd668dd0d75e1e789a
Dawid <dawidnitka@gmail.com>
2024-11-08 14:06:57 +0100

    fix: type cast query parameter in projects API to ensure correct data structure

--- server/api/projects/index.post.ts
+++ server/api/projects/index.post.ts
@@ -1,11 +1,13 @@
 import { createError } from 'h3'
 
+import { Tables } from '~/types/database.types'
+
 import { serverSupabaseClient } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
     const superbaseClient = await serverSupabaseClient(event)
 
-    const query = getQuery(event)
+    const query = getQuery(event) as Tables<'projects'>
     const { data, error } = await superbaseClient
         .from('projects')
         .insert([ query ])
