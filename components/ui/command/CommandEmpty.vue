0a3d6e7f77104a15b4f89658cc6a7facb8210885
Dawid <dawidnitka@gmail.com>
2024-11-11 19:57:37 +0100

    feat: add dialog and popover components with respective templates and props

---
+++ components/ui/command/CommandEmpty.vue
@@ -0,0 +1,25 @@
+<template>
+    <ComboboxEmpty
+        v-bind="delegatedProps"
+        :class="cn('py-6 text-center text-sm', props.class)"
+    >
+        <slot />
+    </ComboboxEmpty>
+</template>
+
+<script setup lang="ts">
+    import { ComboboxEmpty } from 'radix-vue'
+    import { type HTMLAttributes, computed } from 'vue'
+
+    import { cn } from '@/lib/utils'
+
+    import type { ComboboxEmptyProps } from 'radix-vue'
+
+    const props = defineProps<ComboboxEmptyProps & { class?: HTMLAttributes['class'] }>()
+
+    const delegatedProps = computed(() => {
+        const { class: _, ...delegated } = props
+
+        return delegated
+    })
+</script>
---
+++ components/ui/command/CommandSeparator.vue
@@ -0,0 +1,25 @@
+<template>
+    <ComboboxSeparator
+        v-bind="delegatedProps"
+        :class="cn('-mx-1 h-px bg-border', props.class)"
+    >
+        <slot />
+    </ComboboxSeparator>
+</template>
+
+<script setup lang="ts">
+    import { ComboboxSeparator } from 'radix-vue'
+    import { type HTMLAttributes, computed } from 'vue'
+
+    import { cn } from '@/lib/utils'
+
+    import type { ComboboxSeparatorProps } from 'radix-vue'
+
+    const props = defineProps<ComboboxSeparatorProps & { class?: HTMLAttributes['class'] }>()
+
+    const delegatedProps = computed(() => {
+        const { class: _, ...delegated } = props
+
+        return delegated
+    })
+</script>
