3a6c0c28f99d2eaef99c97979ca7dca7acfd5f54
Dawid <dawidnitka@gmail.com>
2024-11-22 02:08:44 +0100

    feat: add experience table and seed data

---
+++ supabase/seeds/experience.sql
@@ -0,0 +1,23 @@
+INSERT INTO public.experience 
+  ("title", "description", "start", "end", "tags", "link")
+VALUES 
+('Senior Frontend Engineer, One Hiring',
+'Planned and led a multi-domain project utilizing Vue 3, Pinia, Vite, Nx, Yarn workspaces, Tailwind, Storybook, and Figma
+
+Implemented integration of Google Analytics (GA) and Google Tag Manager (GTM)
+
+Integrated Lokalise for external translation management, improving localization processes
+
+Refactored structure, code, style, and build systems for existing platforms utilizing Vue 3, Pinia, Vite, Nx, Yarn workspaces, Cypress and SCSS, following industry best practices
+
+Contributed to the creation of a new design system using Figma to standardize UI components
+
+Assessed opportunities and benefits of new technologies, such as Nuxt, UnoCSS, etc.
+
+Communicated effectively with multiple departments and stakeholders (both internal and external)
+
+Documented processes and conducted training sessions for team members on frontend design patterns, Vue, Pinia, and Figma',
+'2023-04-01',
+'2024-12-31',
+'["JavaScript","TypeScript","Vue 3","Pinia","Cypress","Vite","Nx","Tailwind","Scss","Storybook","Figma"]',
+'https://onehiring.com');
