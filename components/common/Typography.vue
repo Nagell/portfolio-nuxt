1b569cbf17ff566f574f8a68a8235cacbfdb61e6
Dawid <dawidnitka@gmail.com>
2024-11-23 02:37:37 +0100

    feat: update typography styles and remove unused SkillBadge component

--- components/common/Typography.vue
+++ components/common/Typography.vue
@@ -27,11 +27,11 @@
 
     const variantClasses = computed(() => {
         const variantStyles: Record<Variant, string> = {
-            h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
-            h2: 'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0',
-            h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
-            h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
-            p: 'leading-7 [&:not(:first-child)]:mt-6',
+            h1: 'scroll-m-20 text-6xl font-medium tracking-tight',
+            h2: 'scroll-m-20 text-4xl font-medium tracking-tight first:mt-0',
+            h3: 'scroll-m-20 text-2xl font-medium tracking-tight',
+            h4: 'scroll-m-20 text-xl font-medium tracking-tight',
+            p: 'leading-6 [&:not(:first-child)]:mt-6',
             ul: 'my-6 ml-6 list-disc [&>li]:mt-2',
             li: 'leading-7',
             code: 'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold'
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
         <CommonSection>
             <CommonTypography
                 tag="h1"
-                class="text-6xl font-medium flex gap-1.5"
+                class="flex gap-1.5"
             >
                 <span class="opacity-0 transition-all animate-[text-blur_1s_forwards_400ms] inline-block will-change-transform">
                     Dawid
@@ -34,7 +34,7 @@
                 tag="p"
                 class="mt-5"
             >
-                <span class="opacity-0 transition-all animate-[text-blur_1s_forwards_900ms] inline-block will-change-transform">
+                <span class="text-muted-foreground opacity-0 transition-all animate-[text-blur_1s_forwards_900ms] inline-block will-change-transform">
                     I’m a seasoned Frontend Developer passionate about creating scalable, user-focused web applications that balance elegant design with robust technology. With expertise in Vue, Nuxt, and TypeScript, I deliver high-performing, maintainable solutions. My experience spans startups and established companies, where I’ve honed skills in responsive design, accessibility, and web performance. Beyond coding, I enjoy mentoring, knowledge sharing, and exploring new technologies to drive innovation.
                 </span>
             </CommonTypography>
@@ -44,9 +44,11 @@
         </CommonSection>
 
         <CommonSection>
-            <h2 class="text-3xl font-bold mb-8">
-                Featured Projects
-            </h2>
+            <CommonTypography
+                tag="h2"
+            >
+                Projects
+            </CommonTypography>
             <div
                 class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
             >
@@ -57,26 +59,10 @@
                 />
             </div>
         </CommonSection>
-
-        <CommonSection>
-            <h2 class="text-3xl font-bold mb-8">
-                My Skills
-            </h2>
-            <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                <FrontSkillBadge
-                    v-for="skill in skills"
-                    :key="skill"
-                    :skill="skill"
-                />
-            </div>
-        </CommonSection>
     </div>
 </template>
 
 <script setup lang="ts">
-    const skills = ref([
-        'JavaScript', 'Vue.js', 'Nuxt.js', 'React', 'Node.js', 'TypeScript', 'TailwindCSS', 'Git'
-    ])
 
     const { data: projects } = await useFetch('/api/projects', {
         headers: useRequestHeaders([ 'cookie' ]),
