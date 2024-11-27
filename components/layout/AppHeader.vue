ccb3cb3f03a15abf16dbe8e2402f363363239626
Dawid <dawidnitka@gmail.com>
2024-11-27 21:55:14 +0100

    feat: update AppHeader navigation links to use NuxtLink for smooth scrolling and improved routing

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -2,40 +2,57 @@
     <NavigationMenu class="flex justify-between items-start">
         <NavigationMenuList>
             <NavigationMenuItem>
-                Logo
+                <NuxtLink
+                    to="/"
+                >
+                    Logo
+                </NuxtLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
                 <NuxtLink
+                    to="/#about"
                     @click.prevent="scrollToAnchor('about')"
                 >
-                    <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+                    <NavigationMenuLink
+                        as="span"
+                        :class="navigationMenuTriggerStyle()"
+                    >
                         About
                     </NavigationMenuLink>
                 </NuxtLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
                 <NuxtLink
+                    to="/#experience"
                     @click.prevent="scrollToAnchor('experience')"
                 >
-                    <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+                    <NavigationMenuLink
+                        as="span"
+                        :class="navigationMenuTriggerStyle()"
+                    >
                         Experience
                     </NavigationMenuLink>
                 </NuxtLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
                 <NuxtLink
+                    to="/#projects"
                     @click.prevent="scrollToAnchor('projects')"
                 >
-                    <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+                    <NavigationMenuLink
+                        as="span"
+                        :class="navigationMenuTriggerStyle()"
+                    >
                         Projects
                     </NavigationMenuLink>
                 </NuxtLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-                <NuxtLink to="/admin/projects">
-                    <NavigationMenuLink :class="navigationMenuTriggerStyle()">
-                        Admin
-                    </NavigationMenuLink>
+                <NuxtLink
+                    to="/admin/projects"
+                    :class="navigationMenuTriggerStyle()"
+                >
+                    Admin
                 </NuxtLink>
             </NavigationMenuItem>
         </NavigationMenuList>
@@ -43,7 +60,7 @@
 </template>
 
 <script setup lang="ts">
-    import { navigationMenuTriggerStyle } from '../ui/navigation-menu'
+    import { navigationMenuTriggerStyle } from '~/components/ui/navigation-menu'
 
     function scrollToAnchor(elementId: string) {
         document.getElementById(elementId)?.scrollIntoView({ behavior: 'smooth' })
