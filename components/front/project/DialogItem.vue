d50d548fe457a263706e417a583945cb9376855f
Dawid <dawidnitka@gmail.com>
2024-12-04 18:13:44 +0100

    feat: enhance DialogItem component to display project tags and improve layout

--- components/front/project/DialogItem.vue
+++ components/front/project/DialogItem.vue
@@ -29,18 +29,26 @@
                 </div>
 
                 <div class="bottom-0 w-full px-6 py-12 flex flex-col gap-10 max-w-[35rem] mx-auto">
+                    <!-- heading -->
                     <CommonTypography
                         variant="h2"
                         tag="h3"
                     >
                         {{ project.title }}
                     </CommonTypography>
+
+                    <!-- description -->
                     <div>
                         <CommonDescription
                             :description="project.description"
                             type="p"
                         />
                     </div>
+
+                    <!-- tags -->
+                    <CommonTagList :tags="project.tags" />
+
+                    <!-- link -->
                     <CommonExternalLink
                         v-if="project.url"
                         :href="project.url"
