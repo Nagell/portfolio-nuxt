754c0b4efa829c8e6bb1c09bbe8d5316ef55b78e
Dawid <dawidnitka@gmail.com>
2024-12-05 03:30:01 +0100

    feat: update image sanitization to replace local development IP with 'localhost'

--- server/sanitizers/projectsQuery.ts
+++ server/sanitizers/projectsQuery.ts
@@ -3,5 +3,8 @@ import { PatchProjectQuery, PostProjectQuery } from '~/types/projects.types'
 export const sanitizeProjectsQuery = (project: PatchProjectQuery | PostProjectQuery) => {
     if (!project.tags) project.tags = []
     if (typeof project.tags === 'string') project.tags = [ ...project.tags.split(',') ]
+    // fix for local development
+    if (project.image?.includes('127.0.0.1')) project.image = project.image.replace('127.0.0.1', 'localhost')
+
     return project
 }
