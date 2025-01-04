deb9dd3ca3a9037024747cc494166655ed112b91
Dawid <dawidnitka@gmail.com>
2025-01-04 21:52:47 +0100

    chore: add Playwright for end-to-end testing and increase timeout for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,8 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
-    test: {}
+    test: {
+        // increased timeout for e2e tests
+        testTimeout: 60000,
+    }
 })
