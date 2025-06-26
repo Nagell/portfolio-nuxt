d2eed60ba29b94e3f61548f8fd53f93a3a778f58
Dawid <dawidnitka@gmail.com>
2025-06-26 17:41:07 +0200

    feat: remove GitHub avatar fetching logic and related types
    - Removed the userAvatar state and the onMounted hook for fetching GitHub avatars.
    - Updated the user data structure to directly use the avatar URL from supabaseUser metadata.
    - Deleted unused GitHub types and utility function for cleaner codebase.

--- utils/github.ts
+++
@@ -1,7 +0,0 @@
-import type { GetGitHubUserResponse } from '~/types/github.types'
-
-export async function getGitHubAvatar(email: string, size: number = 100) {
-    const response = await $fetch<GetGitHubUserResponse>(`https://api.github.com/search/users?q=${email}+in%3Aemail`)
-
-    return response?.items[0]?.avatar_url + `&size=${size}`
-}
