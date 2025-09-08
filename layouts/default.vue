743abf4fe6e42c08324731faa4e36bc7d462c404
Dawid <dawidnitka@gmail.com>
2025-09-08 16:15:51 +0200

    feat: integrate Vercel Speed Insights component
    Added the Speed Insights component to the default layout and updated
    dependencies to include the Vercel Speed Insights package.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="min-h-screen flex flex-col">
+        <SpeedInsights />
         <LayoutAppHeader />
         <main class="flex-grow">
             <slot />
@@ -7,3 +8,6 @@
         <LayoutAppFooter />
     </div>
 </template>
+<script setup lang="ts">
+    import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+</script>
